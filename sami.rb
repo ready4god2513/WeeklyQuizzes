55a3604221315ab5056dfbab457f394b5815229c
Sami Makhuli <smakhuli@yahoo.com>
2012-09-26 14:25:28 -0700

    Solution 2

--- sami.rb
+++ sami.rb
@@ -1,17 +1,8 @@
 (1..100).to_a.each do |number|
   remainder_3 = number % 3
   remainder_5 = number % 5
-  if remainder_3 == 0 && remainder_5 == 0
-    puts "FizzBuzz"
-  else
-    if remainder_3 == 0
-      puts "Fizz"
-    else
-      if remainder_5 == 0
-        puts "Buzz"
-      else
-        puts number
-      end
-    end
-  end
+  puts "FizzBuzz" if remainder_3 == 0 && remainder_5 == 0
+  puts "Fizz" if remainder_3 == 0 && remainder_5 != 0
+  puts "Buzz" if remainder_3 != 0 && remainder_5 == 0
+  puts number if remainder_3 != 0 && remainder_5 != 0
 end
