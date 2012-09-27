03e3f8381204707128ffa1b947d9cff3419769d6
Richard Larcombe <rjlarcombe@gmail.com>
2012-09-26 20:40:45 -0700

    My second attempt after I thought about it some more

--- richard.rb
+++ richard.rb
@@ -1,11 +1,6 @@
 1.upto(100) do |n|
-  if n % 3 == 0 && n % 5 == 0
-    puts "FizzBuzz"
-  elsif n % 3 == 0
-    puts "Fizz"
-  elsif n % 5 == 0        
-    puts "Buzz"
-  else
-    puts n
-  end      
+  what_to_say = ""
+  what_to_say << "Fizz" if n % 3 == 0
+  what_to_say << "Buzz" if n % 5 == 0
+  puts what_to_say.empty? ? n : what_to_say     
 end
